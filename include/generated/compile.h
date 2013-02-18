9659d52c0ab84e4ceb0b3de7cd9cce0e579567dd
Ken <klquicksall@gmail.com>
2013-02-17 22:23:49 -0600

    merge

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Feb 17 19:54:59 CST 2013"
+#define UTS_VERSION "#4 SMP PREEMPT Sun Feb 17 22:00:09 CST 2013"
 #define LINUX_COMPILE_BY "ken"
-#define LINUX_COMPILE_HOST "klquicksall"
+#define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILER "gcc version 4.6.2 20120613 (release) [ARM/embedded-4_6-branch revision 188521] (GNU Tools for ARM Embedded Processors) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.31-Counterfeit-Spring-1.2+"
+#define UTS_RELEASE "3.0.61"
