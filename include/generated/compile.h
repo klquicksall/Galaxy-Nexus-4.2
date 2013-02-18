28b18bd7762e592bb236b3f3a964c778e190098b
Ken <klquicksall@gmail.com>
2013-02-17 23:18:36 -0600

    autogen files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Feb 17 22:00:09 CST 2013"
+#define UTS_VERSION "#5 SMP PREEMPT Sun Feb 17 22:56:57 CST 2013"
 #define LINUX_COMPILE_BY "ken"
 #define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILER "gcc version 4.6.2 20120613 (release) [ARM/embedded-4_6-branch revision 188521] (GNU Tools for ARM Embedded Processors) "
